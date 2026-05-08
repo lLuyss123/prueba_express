6558bde79728c01bc83b75d3f9d962266ba8ee06
luis <lmejiaidep@gmil.com>
2026-05-08 12:17:02 -0500

    Cambios en comentarios

--- utlis.js
+++ utlis.js
@@ -1,7 +1,7 @@
 import fs from "fs"
 const rutaDB = "./db.json"
 export function getData() {
-    //Esta vaina  retorna un string en formato json pero noosotros lo queremos como un objeto. por eso hacemos JSON.paser....
+    //Esta vaina  retorna un string en formato json pero noosotros lo queremos como un objeto JSON. por eso hacemos JSON.paser....
     return JSON.parse(fs.readFileSync(rutaDB));
 }
 
