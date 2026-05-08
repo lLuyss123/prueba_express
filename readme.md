99a6bb4d3974638b20a894841fd06f01a60455ff
luis <lmejiaidep@gmil.com>
2026-05-08 11:56:09 -0500

    Cambiamos algunos nombres de variables para entenderlas}

---
+++ readme.md
@@ -0,0 +1,6 @@
+PRIMERO HAZ NPM INSTALL
+
+package.json se crea con npm init -y
+package-lock.json se crea haciendo npm install express -E
+
+PRIMERO CREAS EL MAIN LUEGO TE VAS A UTILS Y LUEGO CREAS LAS RUTAS
--- utlis.js
+++ utlis.js
@@ -1,11 +1,11 @@
 import fs from "fs"
-const ruta= "./db.json"
-export function getData(){
+const rutaDB = "./db.json"
+export function getData() {
     //Esta vaina  retorna un string en formato json pero noosotros lo queremos como un objeto. por eso hacemos JSON.paser....
-    return JSON.parse(fs.readFileSync(ruta));
+    return JSON.parse(fs.readFileSync(rutaDB));
 }
 
-export function setData(data){
+export function setData(data) {
     //Esta vaina  retorna un JSON  pero noosotros lo queremos como un Objeto. por eso hacemos JSON.stringfy....
-    fs.writeFileSync(ruta,JSON.stringify(data,null,4))
+    fs.writeFileSync(rutaDB, JSON.stringify(data, null, 4))
 }
